9a14a983ec4f5537929285a159cda91e161011af
James Oh <jamesboh@gmail.com>
2017-03-03 09:37:39 -0500

    no issue fix whitespace

--- browser/components/Articles.js
+++ browser/components/Articles.js
@@ -1,54 +1,54 @@
 import React, { Component } from 'react';
 
 export default class Articles extends Component {
-  constructor(props) {
-    super(props)
-    this.infiniteScroll = this.infiniteScroll.bind(this);
-  }
+	constructor(props) {
+		super(props)
+		this.infiniteScroll = this.infiniteScroll.bind(this);
+	}
 
-  componentDidMount () {
-    this.props.onLoadArticles();
-    window.addEventListener('scroll', this.infiniteScroll);
-  }
+	componentDidMount () {
+		this.props.onLoadArticles();
+		window.addEventListener('scroll', this.infiniteScroll);
+	}
 
-  componentDidUpdate() {
-    //this is a fallback for a special case for when the article-wrapper is shorther than window height
-    if (window.innerHeight > document.getElementById('articles-wrapper').scrollHeight) {
-      this.props.nextArticle();
-    }
-  }
+	componentDidUpdate() {
+		//this is a fallback for a special case for when the article-wrapper is shorther than window height
+		if (window.innerHeight > document.getElementById('articles-wrapper').scrollHeight) {
+			this.props.nextArticle();
+		}
+	}
 
-  infiniteScroll () {
-    if (this.props.remainingArticles.length) {
-      if ((window.innerHeight + window.scrollY) >= document.getElementById('articles-wrapper').scrollHeight) {
-        this.props.nextArticle();
-        return
-      } 
-    } else {
-      window.removeEventListener('scroll', this.infiniteScroll);
-    }
-  }
+	infiniteScroll () {
+		if (this.props.remainingArticles.length) {
+			if ((window.innerHeight + window.scrollY) >= document.getElementById('articles-wrapper').scrollHeight) {
+				this.props.nextArticle();
+				return
+			} 
+		} else {
+			window.removeEventListener('scroll', this.infiniteScroll);
+		}
+	}
 
-  render () {
-    return (
-      <section id="articles-wrapper">
-        {
-          this.props.loadedArticles && this.props.loadedArticles.map(article => {
-            return (
-              <article key={article.id}>
-                <h2 dangerouslySetInnerHTML={{ __html: article.title }}></h2>
-                <figure className="hero">
-                	<picture>
-                		<img src={article.hero.url} alt={article.hero.alt} />
-                	</picture>
-                </figure>
-                <div className="item-content" dangerouslySetInnerHTML={{ __html: article.content }}>
-                </div>
-              </article>
-            )
-          })
-        }
-      </section>
-    );
-  }
+	render () {
+		return (
+			<section id="articles-wrapper">
+				{
+					this.props.loadedArticles && this.props.loadedArticles.map(article => {
+						return (
+							<article key={article.id}>
+								<h2 dangerouslySetInnerHTML={{ __html: article.title }}></h2>
+								<figure className="hero">
+									<picture>
+										<img src={article.hero.url} alt={article.hero.alt} />
+									</picture>
+								</figure>
+								<div className="item-content" dangerouslySetInnerHTML={{ __html: article.content }}>
+								</div>
+							</article>
+						)
+					})
+				}
+			</section>
+		);
+	}
 }
--- browser/components/ArticlesContainer.js
+++ browser/components/ArticlesContainer.js
@@ -3,16 +3,16 @@ import { loadArticles, loadNextArticle } from '../action-creators';
 import Articles from './Articles';
 
 const mapStateToProps = (state) => {
-  return {
-    loadedArticles: state.loadedArticles,
-    remainingArticles: state.remainingArticles
-  };
+	return {
+		loadedArticles: state.loadedArticles,
+		remainingArticles: state.remainingArticles
+	};
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onLoadArticles: () => dispatch(loadArticles()),
-    nextArticle: () => dispatch(loadNextArticle())
+		onLoadArticles: () => dispatch(loadArticles()),
+		nextArticle: () => dispatch(loadNextArticle())
   };
 };
 
